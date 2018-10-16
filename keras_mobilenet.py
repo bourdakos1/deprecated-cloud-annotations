99fb8a17e0e5bb601b83e1c17584b442e3e2ee1c
Nick Bourdakos <bourdakos1@gmail.com>
2018-10-16 13:09:18 -0400

    test

--- keras_mobilenet.py
+++ keras_mobilenet.py
@@ -96,8 +96,8 @@ credentials_1 = {
 IMG_WIDTH, IMG_HEIGHT = 224, 224
 EPOCHS = 20
 BATCH_SIZE = 50
-TRAINABLE_LAYERS = 0
-LEARNING_RATE = 0.003
+TRAINABLE_LAYERS = 9
+LEARNING_RATE = 0.0019
 
 
 ################################################################################
