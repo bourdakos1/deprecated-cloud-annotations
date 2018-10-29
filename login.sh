dc1843839f6c75052252dd8161112f5629abf3cb
Nick Bourdakos <bourdakos1@gmail.com>
2018-10-29 15:16:19 -0400

    update bash script

--- login.sh
+++ login.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-read -p 'Resource Instance ID: ' resourceId
-read -sp 'API Key: ' apiKey
+read -p 'Resource Instance ID: ' RESOURCE_ID
+read -sp 'API Key: ' API_KEY
 
-perl -pi -e "s/<key>apiKey<\/key>\n/<key>apiKey<\/key>/" ios-app/Core\ ML\ Vision/Credentials.plist
-perl -pi -e "s/<key>apiKey<\/key>\s*<string>.*<\/string>/<key>apiKey<\/key>\n\t<string>$apiKey<\/string>/" ios-app/Core\ ML\ Vision/Credentials.plist
-perl -pi -e "s/<key>resourceId<\/key>\n/<key>resourceId<\/key>/" ios-app/Core\ ML\ Vision/Credentials.plist
-perl -pi -e "s/<key>resourceId<\/key>\s*<string>.*<\/string>/<key>resourceId<\/key>\n\t<string>$resourceId<\/string>/" ios-app/Core\ ML\ Vision/Credentials.plist
+DATA_COLLECTOR_APP=data-collector/Data\ Collector/Credentials.plist
+INFERENCE_APP=ios-app/Core\ ML\ Vision/Credentials.plist
+PYTHON_TRAINER=.Credentials
 
-perl -pi -e "s/<key>apiKey<\/key>\n/<key>apiKey<\/key>/" data-collector/Data\ Collector/Credentials.plist
-perl -pi -e "s/<key>apiKey<\/key>\s*<string>.*<\/string>/<key>apiKey<\/key>\n\t<string>$apiKey<\/string>/" data-collector/Data\ Collector/Credentials.plist
-perl -pi -e "s/<key>resourceId<\/key>\n/<key>resourceId<\/key>/" data-collector/Data\ Collector/Credentials.plist
-perl -pi -e "s/<key>resourceId<\/key>\s*<string>.*<\/string>/<key>resourceId<\/key>\n\t<string>$resourceId<\/string>/" data-collector/Data\ Collector/Credentials.plist
+API_KEY_REGEX="<key>apiKey<\/key>"
+RESOURCE_ID_REGEX="<key>resourceId<\/key>"
+STRING_REGEX="<string>.*<\/string>"
 
-perl -pi -e "s/API_KEY=.*/API_KEY=$apiKey/" .Credentials
-perl -pi -e "s/RESOURCE_INSTANCE_ID=.*/RESOURCE_INSTANCE_ID=$resourceId/" .Credentials
+
+replacePlistItem () {
+  perl -pi -e "s/$1\n/$1/" "$3"
+  perl -pi -e "s/$1\s*$STRING_REGEX/$1\n\t<string>$2<\/string>/" "$3"
+}
+
+iOSReplace () {
+  replacePlistItem "$API_KEY_REGEX" "$API_KEY" "$1"
+  replacePlistItem "$RESOURCE_ID_REGEX" "$RESOURCE_ID" "$1"
+}
+
+pythonReplace () {
+  perl -pi -e "s/API_KEY=.*/API_KEY=$API_KEY/" "$1"
+  perl -pi -e "s/RESOURCE_INSTANCE_ID=.*/RESOURCE_INSTANCE_ID=$RESOURCE_ID/" "$1"
+}
+
+iOSReplace "$DATA_COLLECTOR_APP"
+iOSReplace "$INFERENCE_APP"
+pythonReplace "$PYTHON_TRAINER"
