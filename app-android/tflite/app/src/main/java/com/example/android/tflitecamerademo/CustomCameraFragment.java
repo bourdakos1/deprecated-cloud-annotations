15870c80b8ee047523ecc06e6a5e1a2ebf8685f4
Nick Bourdakos <bourdakos1@gmail.com>
2018-11-08 15:04:09 -0500

    fix a couple lifecycle bugs

--- app-android/tflite/app/src/main/java/com/example/android/tflitecamerademo/CustomCameraFragment.java
+++ app-android/tflite/app/src/main/java/com/example/android/tflitecamerademo/CustomCameraFragment.java
@@ -32,13 +32,19 @@ import static com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_
 import static com.google.android.material.bottomsheet.BottomSheetBehavior.STATE_HIDDEN;
 
 public class CustomCameraFragment extends CameraFragment {
+    private ImageClassifier mClassifier;
+
     private CameraView mCamera;
     private ImageView mPreview;
     private ViewGroup mCameraLayout;
     private ViewGroup mPreviewLayout;
-    private ImageClassifier mClassifier;
+    private Button mCapture;
+    private Button mClose;
+
     private BottomSheetBehavior mBottomSheetBehavior;
     private ClassificationAdapter mAdapter;
+
+    private boolean mShowCamera;
 
     private View.OnClickListener mBitmapCaptured = (view) -> {
         try {
@@ -64,6 +70,7 @@ public class CustomCameraFragment extends CameraFragment {
     private View.OnClickListener mClosePreview = (view) -> showCamera();
 
     private void showPreview() {
+        mShowCamera = false;
         mCameraLayout.setVisibility(View.GONE);
         mPreviewLayout.setVisibility(View.VISIBLE);
         mBottomSheetBehavior.setHideable(false);
@@ -71,10 +78,38 @@ public class CustomCameraFragment extends CameraFragment {
     }
 
     private void showCamera() {
+        mShowCamera = true;
         mCameraLayout.setVisibility(View.VISIBLE);
         mPreviewLayout.setVisibility(View.GONE);
         mBottomSheetBehavior.setHideable(true);
         mBottomSheetBehavior.setState(STATE_HIDDEN);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mShowCamera = true;
+        try {
+            mClassifier = new ImageClassifier(getContext());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return View.inflate(getContext(), R.layout.fragment_custom_camera, container);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mCamera = view.findViewById(R.id.camera);
+        mPreview = view.findViewById(R.id.image_result);
+        mCameraLayout = view.findViewById(R.id.layout_camera);
+        mPreviewLayout = view.findViewById(R.id.layout_preview);
+        mCapture = view.findViewById(R.id.capture_bitmap);
+        mClose = view.findViewById(R.id.close_preview);
     }
 
     @Override
@@ -96,32 +131,8 @@ public class CustomCameraFragment extends CameraFragment {
         divider.setDrawable(ContextCompat.getDrawable(getContext(), R.drawable.divider));
         recyclerView.addItemDecoration(divider);
 
-        showCamera();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        mCamera = view.findViewById(R.id.camera);
-        mPreview = view.findViewById(R.id.image_result);
-        mCameraLayout = view.findViewById(R.id.layout_camera);
-        mPreviewLayout = view.findViewById(R.id.layout_preview);
-
-        try {
-            mClassifier = new ImageClassifier(getContext());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Button capture = view.findViewById(R.id.capture_bitmap);
-        Button closePreview = view.findViewById(R.id.close_preview);
-        capture.setOnClickListener(mBitmapCaptured);
-        closePreview.setOnClickListener(mClosePreview);
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return View.inflate(getContext(), R.layout.fragment_custom_camera, container);
+        mCapture.setOnClickListener(mBitmapCaptured);
+        mClose.setOnClickListener(mClosePreview);
     }
 
     @Override
@@ -131,8 +142,18 @@ public class CustomCameraFragment extends CameraFragment {
     public void onVideoCaptured(File file) {}
 
     @Override
+    public void onStart() {
+        super.onStart();
+        if (mShowCamera) {
+            showCamera();
+        } else {
+            showPreview();
+        }
+    }
+
+    @Override
     public void onDestroy() {
-        mClassifier.close();
         super.onDestroy();
+        mClassifier.close();
     }
 }
