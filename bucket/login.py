3bbe01f29cd78f062178ca41e671b4d816af5f16
Nick Bourdakos <bourdakos1@gmail.com>
2018-11-15 11:31:49 -0500

    Update login.py

--- bucket/login.py
+++ bucket/login.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from builtins import input
+
 import fileinput
 import getpass
 import re
